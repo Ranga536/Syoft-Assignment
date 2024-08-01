6888390e6aef58e2f8ceef3e62a052c863efcd95
Puli Ranga Kattubadi <126335670+Ranga536@users.noreply.github.com>
2024-08-01 11:02:14 +0530

    Add files via upload

---
+++ Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../App.css'
+
+const Dashboard = ({user}) => {
+  return (
+    <div className="dashboard">
+      <h2>Welcome, {user.user_firstname}!</h2>
+      <p>Email: {user.user_email}</p>
+      <p>Phone: {user.user_phone}</p>
+      <p>City: {user.user_city}</p>
+      <p>Zip Code: {user.user_zipcode}</p>
+    </div>
+  )
+}
+
+export default Dashboard
